4907ee01dd69484c27424509485c65ce6a696e58
anateotfw <ana.aminiiaa@gmail.com>
2023-10-17 09:59:37 +0330

    ‘Update-Oct17’

--- src/Componets/Apps/FAQs/FAQs.js
+++ src/Componets/Apps/FAQs/FAQs.js
@@ -396,7 +396,7 @@ function FAQs(){
                <div className="fixed inset-x-0 bottom-0 p-4">
                    <div className="rounded-lg bg-indigo-600 px-4 py-3 text-white shadow-lg">
                        <p className="text-center text-sm font-medium">
-                           سوالات پر تکرار
+                          سوالات تکراری 
                            <a href="#" className="inline-block underline">
 
                            </a>
